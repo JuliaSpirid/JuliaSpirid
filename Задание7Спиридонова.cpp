c46aef8f9e021eac775c29027916ed48b5b40f58
JuliaSpirid <64611581+JuliaSpirid@users.noreply.github.com>
2020-05-23 17:31:01 +0300

    Add files via upload

---
+++ Задание7Спиридонова.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "MIREA.h"
+using namespace std;
+int main()
+{
+    //Создать МИРЭА
+    MIREA mirea;
+    //Временные объекты типа "студент"
+    Student temp_student("Stud1", "stud1@mirea.ru", "1110-1101", 4, 1);
+    mirea.addStudent(temp_student);
+    Student temp_student2("Stud2", "stud2@mirea.ru", "1110-1101", 4, 2);
+    mirea.addStudent(temp_student2);
+    mirea.printStudents();
+}
